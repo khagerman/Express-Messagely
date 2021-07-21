215a305a5df0891bd25f20097d2044b297b86554
khagerman <71734063+khagerman@users.noreply.github.com>
2021-07-21 16:10:28 -0500

    adding routes

--- routes/messages.js
+++ routes/messages.js
@@ -1,3 +1,8 @@
+const Router = require("express").Router;
+const router = new Router();
+const Message = require("../models/message");
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
+
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -10,7 +15,20 @@
  * Make sure that the currently-logged-in users is either the to or from user.
  *
  **/
-
+router.get("/:id", ensureLoggedIn, async (req, res, next) => {
+  try {
+    const message = await Message.get(req.params.id);
+    if (
+      message.from_user !== req.user.username ||
+      message.to_user !== req.user.username
+    ) {
+      throw new ExpressError("Unauthorized", 401);
+    }
+    return res.json(message);
+  } catch (e) {
+    return next(e);
+  }
+});
 
 /** POST / - post message.
  *
@@ -18,8 +36,15 @@
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
-
-
+router.post("/", ensureLoggedIn, async (req, res, next) => {
+  try {
+    const { from_username, to_username, body } = req.body;
+    const newMsg = await Message.create(from_username, to_username, body);
+    return res.json(newMsg);
+  } catch (e) {
+    return next(e);
+  }
+});
 /** POST/:id/read - mark message as read:
  *
  *  => {message: {id, read_at}}
@@ -27,4 +52,16 @@
  * Make sure that the only the intended recipient can mark as read.
  *
  **/
-
+router.post("/:id/read", ensureLoggedIn, async (req, res, next) => {
+  try {
+    const messageTo = await Message.get(req.params.id);
+    if (messageTo.to_user.username !== req.user.username) {
+      throw new ExpressError("Unauthorized", 401);
+    }
+    let message = await Message.markRead(req.params.id);
+    return res.json(message);
+  } catch (e) {
+    return next(e);
+  }
+});
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,35 @@
+const Router = require("express").Router;
+
+const router = new Router();
+const User = require("../models/user");
+
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
-
+router.get("/", async function (req, res, next) {
+  try {
+    const users = await User.all();
+    return res.json(users);
+  } catch (e) {
+    return next(e);
+  }
+});
 
 /** GET /:username - get detail of users.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
-
-
+router.get("/:username", async (req, res, next) => {
+  try {
+    const user = await User.get(req.params.username);
+    return res.json(user);
+  } catch (e) {
+    return next(e);
+  }
+});
 /** GET /:username/to - get messages to user
  *
  * => {messages: [{id,
@@ -21,8 +39,14 @@
  *                 from_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
-
-
+router.get("/:username/to", async (req, res, next) => {
+  try {
+    const user = await User.messageTo(req.params.username);
+    return res.json(user);
+  } catch (e) {
+    return next(e);
+  }
+});
 /** GET /:username/from - get messages from user
  *
  * => {messages: [{id,
@@ -32,3 +56,12 @@
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+router.get("/:username/from", async (req, res, next) => {
+  try {
+    const user = await User.messageFrom(req.params.username);
+    return res.json(user);
+  } catch (e) {
+    return next(e);
+  }
+});
+module.exports = router;
